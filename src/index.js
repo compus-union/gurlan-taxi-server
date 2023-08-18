f0df7eb9091c29c960990d8b5885741bb004ced2
dasturchioka <dasturchioka@gmail.com>
2023-08-18 16:19:43 +0500

    added mongodb connection function

--- src/index.js
+++ src/index.js
@@ -4,14 +4,23 @@ const cors = require("cors");
 const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
-const dbConfig = require("./configs/db.config")
+const dbConfig = require("./configs/db.config");
+const { initMongoDB } = require("./services/db/mongo.service");
 
 const server = http.createServer(app);
 const io = new Server(server);
 
 app.use(express.json());
 app.use(cors({ origin: "*" }));
-app.use("@", express.static(__dirname))
+app.use("@", express.static(__dirname));
+
+initMongoDB(dbConfig.MONGO_URL)
+  .then((info) => {
+    console.log(info.message);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 io.on("connection", (socket) => {
   console.log("Socket connection set ", Date.now().toLocaleString());
