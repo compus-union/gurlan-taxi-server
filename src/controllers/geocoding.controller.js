4aa459a1c034c5b76adf8b688c7e9dfa74584e50
dasturchioka <dasturchioka@gmail.com>
2024-02-01 16:19:34 +0500

    search place component is design left

--- src/controllers/geocoding.controller.js
+++ src/controllers/geocoding.controller.js
@@ -1,4 +1,3 @@
-const { PrismaClient } = require("@prisma/client");
 const {
   searchPlaceService,
 } = require("../services/geocoding/geocoding.service");
@@ -8,12 +7,15 @@ async function searchPlace(req, res) {
     const { place } = req.params;
 
     const response = await searchPlaceService(place);
-
     if (!response) {
       return res.json({
         status: "no-connection",
         msg: "Couldn't get data from OSM",
       });
+    }
+
+    if (!response.length) {
+      return res.json({ status: "not-found", msg: "Joy topilmadi" });
     }
 
     return res.json({ status: "ok", data: response });
