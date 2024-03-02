a95d3b78f88f3a3a4122fa66954446694c695e1e
dasturchioka <dasturchioka@gmail.com>
2024-03-02 15:46:14 +0500

    calculate price function removed & added distance converter service

--- src/services/price/calculatePrice.service.js
+++ src/services/price/calculatePrice.service.js
@@ -1,16 +1,5 @@
 async function calculateInitialPrice(meters) {
-  const km = meters / 1000;
-  const kmFixed = km.toFixed(1) + " km";
 
-  async function fixKm(km) {
-    return { km: km.toFixed(1) + " km" };
-  }
-
-  return {
-    km,
-    kmFixed,
-    fixKm,
-  };
 }
 
 module.exports = { calculateInitialPrice };
