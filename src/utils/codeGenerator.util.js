e8f9b0fe9085c6a7fed83c9ecb7ef53080c88322
dasturchioka <dasturchioka@gmail.com>
2024-04-10 15:44:30 +0500

    eslint worked

--- src/utils/codeGenerator.util.js
+++ src/utils/codeGenerator.util.js
@@ -3,14 +3,16 @@ async function generateConfirmationCode(length) {
   let max = Math.pow(10, length) - 1;
   let result = Math.floor(Math.random() * (max - min + 1)) + min;
   console.log(result.toString());
-  return result.toString()
+  return result.toString();
 }
 
 async function generatePromocode(length) {
-  const characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  let promoCode = '';
+  const characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let promoCode = "";
   for (let i = 0; i < length; i++) {
-      promoCode += characters.charAt(Math.floor(Math.random() * characters.length));
+    promoCode += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    );
   }
   return promoCode;
 }
