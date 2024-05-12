b7f71449009b18c3f5cc0f3d27dfc30433a0ef56
dasturchioka <dasturchioka@gmail.com>
2024-05-12 15:47:36 +0500

    resources (imgs of plans) replaced into local folder

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ const server = http.createServer(app);
 const io = new Server(server);
 global.io = io;
 
+app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.use(compression());
 app.use(cors({ origin: "*" }));
